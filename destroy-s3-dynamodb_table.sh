f182916d3c73ba78dec5bd3bd41ed589ae84b9e4
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-24 03:53:12 +0530

    Update destroy-s3-dynamodb_table.sh

--- destroy-s3-dynamodb_table.sh
+++ destroy-s3-dynamodb_table.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+echo "🔧 Step 1: Downloading terraform.tfstate from S3..."
+aws s3 cp s3://terraform-backend-faisal-khan/s3/terraform.tfstate ./terraform.tfstate
+
 echo "🔧 Step 2: Initializing Terraform locally..."
 terraform init -reconfigure
 
@@ -8,6 +13,9 @@ terraform validate
 
 echo "📝 Step 4: Formatting Terraform files..."
 terraform fmt -recursive
+
+echo "🔍 Step 5: Showing plan for destroy..."
+terraform plan -destroy -var-file="terraform.tfvars"
 
 echo "🛑 WARNING: This will permanently destroy the S3 bucket and DynamoDB table!"
 read -p "Type 'destroy' to continue: " confirm
