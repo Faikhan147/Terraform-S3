580d78fb014d40bd61f7fc4664bfdbbb76b4db98
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 14:33:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.terraform_backend.id
+  value = aws_s3_bucket.private_bucket.id
 }
 
-output "dynamodb_table_names" {
-  value = [for t in aws_dynamodb_table.terraform_lock : t.name]
+output "dynamodb_tables" {
+  value = [for t in aws_dynamodb_table.tables : t.name]
 }
