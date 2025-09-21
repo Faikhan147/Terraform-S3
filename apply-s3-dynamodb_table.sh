2d50b731748e66fc2828e14d0dd82fd7ecaaaa62
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 16:12:06 +0530

    Update apply-s3-dynamodb_table.sh

--- apply-s3-dynamodb_table.sh
+++ apply-s3-dynamodb_table.sh
@@ -4,14 +4,8 @@ set -u
 
 VAR_FILE="terraform.tfvars"
 
-echo "🔹 Step 1: Apply S3 bucket and DynamoDB tables locally first..."
-terraform init
-terraform apply -auto-approve -var-file="$VAR_FILE"
-
-echo "🔹 Step 2: Initialize backend with the created S3 bucket..."
-terraform init -backend-config=backend-prod.hcl -reconfigure
-
-echo "🔹 Step 3: Apply again to migrate state to backend..."
+echo "🔹 Step 1: Initialize Terraform and apply resources..."
+terraform init -reconfigure
 terraform apply -auto-approve -var-file="$VAR_FILE"
 
 echo "✅ Deployment completed successfully!"
