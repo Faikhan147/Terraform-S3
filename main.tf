6df8f3dd361df452c7ade165ccaf4f3550b0844a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 22:49:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,8 +32,6 @@ resource "aws_dynamodb_table" "tables" {
 
   name           = each.key
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = var.dynamodb_read_capacity
-  write_capacity = var.dynamodb_write_capacity
   hash_key       = "LockID"
 
   attribute {
