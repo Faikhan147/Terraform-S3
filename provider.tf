78cc4153f700b016a9420357a91cb502a53746a9
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 14:58:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # Backend configured via backend-prod.hcl
+}
+
 provider "aws" {
   region = var.region
 }
