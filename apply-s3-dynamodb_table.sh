f0d852884a9786c5e55e8844f424aac25645134c
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 15:02:56 +0530

    Update apply-s3-dynamodb_table.sh

--- apply-s3-dynamodb_table.sh
+++ apply-s3-dynamodb_table.sh
@@ -3,15 +3,15 @@ set -e
 set -u
 
 echo "🔹 Initializing Terraform backend..."
-terraform init -backend-config=../backend-prod.hcl -reconfigure
+terraform init -backend-config=backend-prod.hcl -reconfigure
 
 echo "🔹 Validating Terraform configuration..."
 terraform validate
 
 echo "🔹 Planning Terraform changes..."
-terraform plan -var-file=../terraform.tfvars
+terraform plan -var-file=terraform.tfvars
 
 echo "🔹 Applying Terraform changes..."
-terraform apply -auto-approve -var-file=../terraform.tfvars
+terraform apply -auto-approve -var-file=terraform.tfvars
 
 echo "✅ Deployment completed successfully!"
