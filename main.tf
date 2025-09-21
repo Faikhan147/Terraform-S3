3f4d7c50e113ed26a8ed7bb4961b9ca35cb4d058
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 06:23:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,14 +75,3 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
-
-# ------------------------------
-# Outputs
-# ------------------------------
-output "dynamodb_table_names" {
-  value = [for k, t in aws_dynamodb_table.terraform_lock : t.name if k == terraform.workspace]
-}
-
-output "s3_bucket_name" {
-  value = terraform.workspace == "prod" ? aws_s3_bucket.terraform_backend[0].bucket : "terraform-backend-all-env"
-}
