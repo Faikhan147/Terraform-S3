b5e721e11dd5e4003e2ccb045769e70728f826a6
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 05:30:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
 resource "aws_s3_bucket" "terraform_backend" {
-  bucket = var.bucket_name
-
+  bucket        = var.bucket_name
   force_destroy = true
 
   tags = {
     Name = "Terraform Backend Bucket"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.terraform_backend.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
@@ -33,13 +23,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
       kms_master_key_id = var.kms_key_arn
       sse_algorithm     = "aws:kms"
     }
-
     bucket_key_enabled = true
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.terraform_backend.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = var.dynamodb_table_name
+  count        = length(var.dynamodb_tables)
+  name         = var.dynamodb_tables[count.index]
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
