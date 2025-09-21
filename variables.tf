c3e20d6d3d556f9fc479ae34a3c0327b74e47e6a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 13:24:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,4 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "bucket_name" {
-  description = "S3 bucket name for Terraform backend"
-  type        = string
-  default     = "terraform-backend-all-env"
-}
-
-variable "dynamodb_tables" {
-  description = "List of DynamoDB lock tables"
-  type        = list(string)
-  default     = [
-    "terraform-locks-prod",
-    "terraform-locks-stage",
-    "terraform-locks-qa"
-  ]
-}
-
-variable "kms_key_arn" {
-  description = "KMS Key ARN for S3 encryption"
-  type        = string
-  default     = "arn:aws:kms:ap-south-1:923884399206:alias/aws/s3"
+  type    = string
+  default = "ap-south-1"
 }
