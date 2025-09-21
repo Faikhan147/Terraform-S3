496e69bc7835457c64947f68fa6c05f0c535c19e
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 14:37:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,4 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
-
+# Provider configuration only
 provider "aws" {
   region = var.region
 }
