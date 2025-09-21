64e78bf888223c84a0e656559c9079bf39616ea2
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 16:11:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  # Backend configured later using backend-prod.hcl
+  backend "s3" {
+    bucket         = "my-private-bucket-ap-south-1"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    kms_key_id     = "arn:aws:kms:ap-south-1:923884399206:alias/aws/s3"
+    encrypt        = true
+  }
 }
