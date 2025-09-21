bcb6b20826d737143c5b140f0b56a1ecf998a008
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 14:33:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,22 @@
 variable "region" {
-  type    = string
   default = "ap-south-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Private S3 bucket name"
+  type        = string
+}
+
+variable "dynamodb_tables" {
+  description = "List of DynamoDB table names"
+  type        = list(string)
+  default     = ["table1", "table2", "table3"]
+}
+
+variable "dynamodb_read_capacity" {
+  default = 5
+}
+
+variable "dynamodb_write_capacity" {
+  default = 5
 }
