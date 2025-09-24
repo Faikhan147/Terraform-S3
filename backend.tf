046e2edc72a92becc1abb73830424dbd9045a51a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-24 17:46:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-all-env"
     key            = "s3/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     kms_key_id     = "arn:aws:kms:ap-south-1:923884399206:alias/aws/s3"
     encrypt        = true
   }
