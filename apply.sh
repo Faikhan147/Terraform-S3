0481a001874a1646e689aa92beb6f400edd9588f
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-04 16:46:58 +0530

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -20,6 +20,10 @@ terraform validate
 echo "📝 Formatting Terraform files..."
 terraform fmt -recursive
 
+# Display workspace list
+echo "🔢 Listing available workspaces..."
+terraform workspace list
+
 echo "📄 Creating plan..."
 terraform plan -var-file="terraform.tfvars" -out=tfplan.out
 
