3a9a1d29eed9c2a672dd3ffcd76eb1696dc6420f
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-25 22:13:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-all-envs"
+    bucket         = "terraform-backend-all-envss"
     key            = "s3/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
